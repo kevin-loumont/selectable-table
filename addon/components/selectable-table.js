4b386aaee89d9ffedd8f2ad89ae1ee8a05fe3e97
kevin loumont <loumont.kevin@gmail.com>
2021-10-06 17:24:00 +0200

    feat: store row selected

--- addon/components/selectable-table.js
+++ addon/components/selectable-table.js
@@ -1,7 +1,9 @@
 import Component from '@glimmer/component'
-
+import { tracked } from '@glimmer/tracking'
+import { action } from '@ember/object';
 
 export default class SelectableTable extends Component {
+  @tracked itemsSelected = []
 
   get tableTitles() {
     if (Array.isArray(this.args.model) && this.args.model.length > 0)
@@ -9,5 +11,13 @@ export default class SelectableTable extends Component {
       return Object.getOwnPropertyNames(this.args.model[ 0 ])
     }
     return []
+  }
+
+  @action
+  onClickCheckbox(index) {
+    this.itemsSelected = !this.itemsSelected.includes(index)
+    ? [ ...this.itemsSelected, index ]
+    : this.itemsSelected.filter((i) => index !== i)
+
   }
 }
